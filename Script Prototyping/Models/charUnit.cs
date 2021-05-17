b8f6c9849d2fa413c0479dcf60b3eebd14377a20
Joseph Giunta <jgnohack@gmail.com>
2021-05-17 15:20:10 -0700

    add basic unit stat file

---
+++ Script Prototyping/Models/charUnit.cs
@@ -0,0 +1,21 @@
+namespace prototyping.Models
+{
+  public class charUnit
+  {
+    public string unitName { get; set; }
+    public string unitClass { get; set; }
+    public int unitLvl { get; set; }
+    public int unitExp { get; set; }
+    public int unitMaxHealth { get; set; }
+    public int unitCurrentHealth { get; set; }
+    public int unitBaseMovement { get; set; }
+    public int unitStrength { get; set; }
+    public int unitDefense { get; set; }
+    public int unitResistance { get; set; }
+    public int unitMagic { get; set; }
+    public int unitSkill { get; set; }
+    public int unitSpeed { get; set; }
+    public int unitLuck { get; set; }
+
+  }
+}
