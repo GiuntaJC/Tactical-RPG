3d5e3440f88b28d3d8a091c41c973a33ffbb3b84
Joseph Giunta <jgnohack@gmail.com>
2021-05-18 11:41:42 -0700

    practice adding scripts and stats to game object

---
+++ Unity/Tactical RPG/Assets/unitStats.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unitStats : MonoBehaviour
+{
+    public int unitHealth;
+	public int unitStrength;
+    public int unitDefense;
+    public int unitResistance;
+    public int unitMagic;
+    public int unitSkill;
+    public int unitSpeed;
+    public int unitLuck;
+}
